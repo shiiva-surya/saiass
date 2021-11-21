e08eda68e1913b66fc7e9546384511e7a311354f
shiiva-surya <46842852+shiiva-surya@users.noreply.github.com>
2021-11-21 19:01:25 +0530

    Add files via upload

---
+++ dc.py
@@ -0,0 +1,34 @@
+a=float(input())
+b=float(input())
+c=int(input())
+d=b/a
+for i in range(3,c):
+    b=b*d
+print(round(b,3))
+
+
+
+
+
+
+
+
+d=input2//input1
+if input3==1:
+    c=input1//d
+    c=round(c,3)
+    return c
+elif input3==2:
+    c=round(input1,3)
+    return c
+
+
+
+for i in range(3,input3):
+    input2=input2*d
+
+
+
+    
+c=round(input2,3)
+return c
